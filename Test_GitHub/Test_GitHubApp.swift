0fcb0fd6df9ce9eef1b23b6d134b889c0ae60b51
Loïc J <info.bubulle@free.fr>
2022-10-20 10:50:37 +0200

    Initial Commit

---
+++ Test_GitHub/Test_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_GitHubApp.swift
+//  Test_GitHub
+//
+//  Created by apprenant96 on 20/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Test_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
